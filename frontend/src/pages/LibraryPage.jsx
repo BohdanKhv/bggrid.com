7b4f4dec958a9da474cbe1ade3c92e3a4e17ee4c
Bohdan Khvorostovskyi <bohdankhv@gmail.com>
2024-11-26 19:38:57 -0500

    Refactor user authentication flow: streamline login process, enhance error handling, and improve session management

--- frontend/src/pages/LibraryPage.jsx
+++ frontend/src/pages/LibraryPage.jsx
@@ -1,71 +1,126 @@
 import { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getMyLibrary } from '../features/library/librarySlice'
-import {Avatar, Button, ErrorInfo, HorizontalScroll, IconButton, InputSearch, Image, Icon} from '../components'
+import {Avatar, Button, ErrorInfo, HorizontalScroll, IconButton, InputSearch, Image, Icon, Dropdown} from '../components'
 import { Link, useNavigate, useSearchParams } from 'react-router-dom'
-import { closeIcon, editIcon, gamesIcon, linkIcon, diceIcon, searchIcon, starFillIcon, weightIcon, usersIcon, usersFillIcon, bellIcon } from '../assets/img/icons'
+import { closeIcon, editIcon, gamesIcon, linkIcon, diceIcon, searchIcon, starFillIcon, weightIcon, usersIcon, usersFillIcon, bellIcon, rightArrowIcon, moreIcon, upArrowRightIcon, listIcon, gridIcon, arrowUpShortIcon, arrowDownShortIcon } from '../assets/img/icons'
 import { tagsEnum } from '../assets/constants'
 import { numberFormatter } from '../assets/utils'
 import GameSearchModal from './game/GameSearchModal'
 import UpdateLogPlay from './game/UpdateLogPlay'
+import { DateTime } from 'luxon'
 
-const LibraryItem = ({ item }) => {
+const LibraryItem = ({ item, hideInfo }) => {
 
     const [searchParams, setSearchParams] = useSearchParams()
 
+    const { user } = useSelector((state) => state.auth)
+
     return (
-        <div className="border-radius px-sm-3 pt-4 transition-duration animation-slide-in display-on-hover-parent">
+        <div className="border-radius px-sm-3 transition-duration animation-slide-in display-on-hover-parent">
             <div className="flex justify-between"
-                onClick={(e) => {
-                    e.stopPropagation()
-                    searchParams.set('play', item.game._id)
-                    setSearchParams(searchParams)
-                }}
             >
-            <div className="flex gap-3 flex-1">
-                <Image
-                    img={item?.game?.thumbnail}
-                    classNameContainer="w-set-100-px h-set-100-px border-radius"
-                    classNameImg="border-radius"
-                />
-                <div className="flex flex-col justify-between flex-1">
-                    <Link className="fs-16 weight-600 pointer text-underlined-hover"
-                        to={`/g/${item.game._id}`}
-                        target='_blank'
+                <div className="flex gap-3 flex-1 py-3">
+                    <div
+                        className="flex justify-center align-center px-3 opacity-50 hover-opacity-100 clickable"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            searchParams.set('logPlay', item.game._id)
+                            setSearchParams(searchParams)
+                        }}
                     >
-                        {item.game.name}
-                    </Link>
-                    <div className="fs-12 text-secondary">
-                        {item.game.yearPublished}
-                    </div>
-                    <div className="flex gap-2 align-center mt-3">
-                        <Button
-                            label="Review"
-                            icon={editIcon}
-                            variant="secondary"
-                            type="outline"
-                            onClick={(e) => {
-                                e.stopPropagation()
-                                searchParams.set('addGame', item.game._id)
-                                setSearchParams(searchParams)
-                            }}
-                        />
-                        <Button
-                            label="Log a Play"
+                        <Icon
                             icon={diceIcon}
-                            variant="secondary"
-                            type="filled"
-                            className="display-on-hover display-on-hover-sm-block"
-                            onClick={(e) => {
-                                e.stopPropagation()
-                                searchParams.set('logPlay', item.game._id)
-                                setSearchParams(searchParams)
-                            }}
+                            size="md"
                         />
+                    </div>
+                    <Image
+                        img={item?.game?.thumbnail}
+                        classNameContainer="w-set-50-px h-set-50-px border-radius"
+                        classNameImg="border-radius"
+                    />
+                    <div className="flex flex-col justify-between flex-1">
+                        <div className="flex justify-between gap-3">
+                            <div className="flex flex-col">
+                        <div className="flex gap-2">
+                                <Link className="fs-16 text-underlined-hover w-fit-content text-ellipsis-1 h-fit-content"
+                                    to={`/g/${item.game._id}`}
+                                    target='_blank'
+                                    >
+                                    {item.game.name}
+                                </Link>
+                                        <div className="flex align-center  border-radius gap-1 pointer"
+                                            onClick={(e) => {
+                                                e.stopPropagation()
+                                                searchParams.set('addGame', item.game._id)
+                                                setSearchParams(searchParams)
+                                            }}
+                                        >
+                                            <Icon icon={starFillIcon} size="sm" className={`${item.rating === 10 ? 'fill-primary' : item.rating >= 7 ? 'fill-success' : item.rating >= 5 ? 'fill-warning' : 'fill-danger'}`}
+                                            />
+                                            <span className={`fs-14 weight-600 ${item.rating === 10 ? 'text-primary' : item.rating >= 7 ? 'text-success' : item.rating >= 5 ? 'text-warning' : 'text-danger'}`}
+                                            >{item.rating || 0}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            <Dropdown
+                                classNameDropdown="p-0 border-radius-sm bg-tertiary"
+                                customDropdown={<IconButton
+                                    icon={moreIcon}
+                                    className="display-on-hover display-on-hover-sm-block"
+                                    variant="secondary"
+                                    dataTooltipContent="More"
+                                    type="link"
+                                    muted
+                                    size="sm"
+                                />}
+                            >
+                                <Button
+                                    label="Update Library"
+                                    icon={editIcon}
+                                    variant="secondary"
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        searchParams.set('addGame', item.game._id)
+                                        setSearchParams(searchParams)
+                                    }}
+                                    className="w-100 border-radius-none justify-start weight-400"
+                                />
+                                <Button
+                                    label="Log play"
+                                    icon={diceIcon}
+                                    variant="secondary"
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        searchParams.set('logPlay', item.game._id)
+                                        setSearchParams(searchParams)
+                                    }}
+                                    className="w-100 border-radius-none justify-start weight-400"
+                                />
+                                <Button
+                                    label="Go to game page"
+                                    icon={upArrowRightIcon}
+                                    variant="secondary"
+                                    to={`/g/${item.game._id}`}
+                                    className=" border-radius-none justify-start weight-400"
+                                />
+                            </Dropdown>
+                        </div>
+                        <div className="flex justify-between gap-3">
+                            <div className="fs-12 text-secondary text-ellipsis-1">
+                                Last played: {item.lastPlayed ? DateTime.now().diff(DateTime.fromISO(item.playDate), ['days']).days > 1 ? DateTime.fromISO(item.playDate).toFormat('LLL dd') :
+                                DateTime.fromISO(item.playDate).toRelative().replace(' days', 'd').replace(' day', 'd').replace(' hours', 'h').replace(' hour', 'h').replace(' minutes', 'm').replace(' minute', 'm').replace(' seconds', 's').replace(' second', 's') : 'never'}
+                            </div>
+                            <div className="fs-12 text-secondary">
+                                {item.plays || 0} plays
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        {hideInfo ? 
+            <div className="border-bottom flex gap-3"/>
+        :
             <HorizontalScroll
                 className="mt-5 border-bottom pb-4"
             >
@@ -127,6 +182,7 @@ const LibraryItem = ({ item }) => {
                     </span>
                 </div>
             </HorizontalScroll>
+        }
         </div>
     )
 }
@@ -139,6 +195,8 @@ const LibraryPage = () => {
     const { library, isLoading, msg } = useSelector((state) => state.library)
     const [searchValue, setSearchValue] = useState('')
     const [tags, setTags] = useState([])
+    const [sortBy, setSortBy] = useState('dateAdded')
+    const [sortOrder, setSortOrder] = useState('desc')
 
     const { user } = useSelector((state) => state.auth)
 
@@ -209,7 +267,7 @@ const LibraryPage = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="sticky top-0 bg-main py-3 px-sm-3">
+                        <div className="sticky top-0 bg-main py-3 px-sm-3 z-3">
                             <HorizontalScroll>
                                 {tags.length > 0 ? (
                                     <IconButton
@@ -250,6 +308,66 @@ const LibraryPage = () => {
                                 ))}
                             </HorizontalScroll>
                         </div>
+                        <div className="px-sm-3 py-3 flex justify-between align-center">
+                            <Dropdown
+                                label="Relevance"
+                                classNameContainer="p-0 border-none bold"
+                                widthUnset
+                                customDropdown={
+                                    <>
+                                    <Button
+                                        type="secondary"
+                                        variant="link"
+                                        label={
+                                            <>
+                                            <span className="weight-400">Sort by: </span>
+                                            <strong>
+                                                {sortBy === 'dateAdded' ? 'Date Added' : sortBy === 'rating' ? 'Rating' : 'Plays'} {sortOrder === 'asc' ? '↓' : '↑'}
+                                            </strong>
+                                            </>
+                                        }
+                                    />
+                                    </>
+                                }
+                            >
+                                <Button
+                                    borderRadius="sm"
+                                    label="Date Added"
+                                    className="justify-start"
+                                    variant="text"
+                                    iconRight={sortBy === 'dateAdded' ? sortOrder === 'asc' ? arrowDownShortIcon : arrowUpShortIcon : null}
+                                    onClick={() => {
+                                        setSortBy('dateAdded')
+                                        if (sortOrder === 'asc') setSortOrder('desc')
+                                        else setSortOrder('asc')
+                                    }}
+                                />
+                                <Button
+                                    borderRadius="sm"
+                                    className="justify-start"
+                                    variant="text"
+                                    label="Rating"
+                                    iconRight={sortBy === 'rating' ? sortOrder === 'asc' ? arrowDownShortIcon : arrowUpShortIcon : null}
+                                    onClick={() => {
+                                        setSortBy('rating')
+                                        if (sortOrder === 'asc') setSortOrder('desc')
+                                        else setSortOrder('asc')
+                                    }}
+                                />
+                                <Button
+                                    borderRadius="sm"
+                                    className="justify-start"
+                                    variant="text"
+                                    label="Plays"
+                                    iconRight={sortBy === 'plays' ? sortOrder === 'asc' ? arrowDownShortIcon : arrowUpShortIcon : null}
+                                    onClick={() => {
+                                        setSortBy('plays')
+                                        if (sortOrder === 'asc') setSortOrder('desc')
+                                        else setSortOrder('asc')
+                                    }}
+                                />
+                            </Dropdown>
+                        </div>
                         <div className="pb-6">
                             {library.length > 0 && !isLoading ? (
                                 <div className="flex flex-col">
@@ -259,8 +377,17 @@ const LibraryPage = () => {
                                     if (tags.length === 0) return true
                                     return tags.some((tag) => item.tags.includes(tag))
                                 })
+                                .sort((a, b) => {
+                                    if (sortBy === 'dateAdded') {
+                                        return sortOrder === 'asc' ? DateTime.fromISO(a.createdAt) - DateTime.fromISO(b.createdAt) : DateTime.fromISO(b.createdAt) - DateTime.fromISO(a.createdAt)
+                                    } else if (sortBy === 'rating') {
+                                        return sortOrder === 'asc' ? a.rating - b.rating : b.rating - a.rating
+                                    } else {
+                                        return sortOrder === 'asc' ? a.plays - b.plays : b.plays - a.plays
+                                    }
+                                })
                                 .map((item) =>
-                                    <LibraryItem key={item._id} item={item} />
+                                    <LibraryItem key={item._id} item={item} hideInfo />
                                 )}
                                 
                                 {library
